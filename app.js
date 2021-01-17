5913a2818bcad9a8742a73fe0e618615a91d1643
chandana <chandanamj325@gmail.com>
2021-01-17 11:14:11 +0530

    alert prompt

---
+++ app.js
@@ -0,0 +1,13 @@
+// console.log('external script running');
+
+// alert('this script wrks')
+
+// input
+var userName = prompt('May i know your name')
+
+// processing
+var greeting = 'welcome ' + userName
+
+// output
+alert(greeting);
+
