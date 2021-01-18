0e4d93de0b87e21d4bc37491285d295507bd1fc4
chandana <chandanamj325@gmail.com>
2021-01-18 20:18:36 +0530

    fetch done

--- app.js
+++ app.js
@@ -7,13 +7,42 @@ var btnTranslate = document.querySelector('#btn-translate')
 // ll give output
 var outputDiv = document.querySelector('#output');
 
+const serverURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json'
 
+// ?text=testing
+
+function getTranslationURL(text) {
+  return serverURL + '?' + 'text=' + text
+}
 
 function clickHandler() {
-  outputDiv.innerText= 'translated:' + txtInput.value
+  // outputDiv.innerText= 'translated:' + txtInput.value
+
+  let inputText = txtInput.value
+  fetch(getTranslationURL(inputText))
+    .then(res => res.json())
+    .then(json => console.log(json.contents.translated))
 }
 
 btnTranslate.addEventListener('click', clickHandler)
+
+
+
+// var text1 = 'I am Iron Man'
+// undefined
+// var text2 = 'I am Shaktimaan'
+// undefined
+// var text3 = 'I am Batman'
+// undefined
+// function constructURL(text) {
+//     return 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json' + '?text=' + text }
+// undefined
+// constructURL(text1)
+// "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Iron Man"
+// function doFetch(text) {
+//     fetch(constructURL(text)) .then(response => response.json()) .then(json => console.log(json)) }
+// undefined
+// doFetch(text1)
 
 
 
