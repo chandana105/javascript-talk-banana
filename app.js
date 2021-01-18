d3d4d00a620775ad0c5ee73b137402970ee317f4
chandana <chandanamj325@gmail.com>
2021-01-18 11:36:41 +0530

    liv exercise with querySelector

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var btnTranslate = document.querySelector('#btn-translate')
 var txtInput = document.querySelector('#txt-input')
+var btnPrimary = document.querySelector('.btn-primary') 
+var inputTranslator = document.querySelector('input[name="translator"]')
+var textArea = document.querySelector("textarea");
+// var textArea = document.querySelectorAll("textarea"); //all
+
 
 console.log(btnTranslate);
 console.log(txtInput);
+console.log(btnPrimary);
+console.log(inputTranslator);
+console.log(textArea);
 
 
 
